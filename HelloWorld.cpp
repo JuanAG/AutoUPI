7d074959b59382ccb5ba014d2644b8c3ff2fb46e
jalcan <juaalcgui@alum.us.es>
2016-11-03 14:13:38 +0100

    Qt Works

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,6 +1,11 @@
-#include <stdio.h>
+#include <QApplication>
+#include <QDebug>
 
-int main(){
-    printf("Hello world 1");
+int main(int argc, char *argv[])
+{
+    //QCoreApplication a(argc, argv);
+
+    qDebug() << QT_VERSION_STR << "The Qt libraries works";
+
     return 0;
 }
